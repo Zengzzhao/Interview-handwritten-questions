5bd684647f8b49ae8a627ad1402a0da4e2db466d
Zengzzhao <787332942@qq.com>
2025-10-03 11:18:39 +0800

    docs: 深对比

--- js/深比对/index.js
+++ js/深比对/index.js
@@ -1,9 +1,10 @@
 function deepEqual(a, b) {
-    // 基本类型直接比较
+    // 基本类型比较，如果a,b都是基本类型且相等则返回true
     if (a === b) return true
-    // 处理null/undefined/NaN 和 非对象类型
-    if (a == null || b == null || typeof a !== 'object' || typeof b !== 'object') return false
-    // 统一比较数组与对象(数组数组、对象对象、数组对象)
+    // 否则说明a,b都是基本类型但值不相等 或 a,b不都是基本类型是引用object类型
+    // 对于a,b都是基本类型但值不相等的情况直接返回false
+    if (typeof a !== 'object' || typeof b !== 'object') return false
+    // 对于a,b不都是基本类型是引用object类型，统一比较数组与对象(数组数组、对象对象、数组对象)
     const keysA = Object.keys(a)
     const keysB = Object.keys(b)
     return keysA.length === keysB.length && keysA.every(key => deepEqual(a[key], b[key]))
