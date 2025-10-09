4ec74511058b9a32ea4df72b20aa700c7355cc52
Zengzzhao <787332942@qq.com>
2025-10-09 14:39:54 +0800

    fix: 深比对重构

--- js/深比对/index.js
+++ js/深比对/index.js
@@ -1,10 +1,9 @@
 function deepEqual(a, b) {
-    // 基本类型比较，如果a,b都是基本类型且相等则返回true
+    // a/b不是对象，直接返回比较结果
+    if (typeof a !== 'object' || typeof b !== 'object') return a === b
+    // a,b都是对象，且地址相同返回true
     if (a === b) return true
-    // 否则说明a,b都是基本类型但值不相等 或 a,b不都是基本类型是引用object类型
-    // 对于a,b都是基本类型但值不相等的情况直接返回false
-    if (typeof a !== 'object' || typeof b !== 'object') return false
-    // 对于a,b不都是基本类型是引用object类型，统一比较数组与对象(数组数组、对象对象、数组对象)
+    // a,b都是对象，但地址不同，递归比较每个值是否相同
     const keysA = Object.keys(a)
     const keysB = Object.keys(b)
     return keysA.length === keysB.length && keysA.every(key => deepEqual(a[key], b[key]))
