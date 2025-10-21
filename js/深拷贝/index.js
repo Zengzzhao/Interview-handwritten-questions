1c103defb6a3427c321566b3771247bfec2d0897
Zengzzhao <787332942@qq.com>
2025-10-21 10:44:48 +0800

    feat: js手写之深浅拷贝

--- js/深拷贝/index.js
+++ js/深拷贝/index.js
@@ -1,23 +1,30 @@
 function deepClone(target) {
     let obj
     // 处理引用类型
-    if (typeof obj === 'object') {
-        // 处理数组
-        if (Array.isArray(target)) {
-            obj = []
-            for (const item of target) {
-                obj.push(deepClone(item))
-            }
-        } else if (target == null) {
-            obj = null
-        } else { // 处理对象
-            obj = {}
-            for (const prop in target) {
-                obj[prop] = deepClone(target[prop])
-            }
+    if (typeof target === 'object' && target !== null) {
+        obj = Array.isArray(target) ? [] : {}
+        for (const key in target) {
+            obj[key] = deepClone(target[key])
         }
     }
-    // 处理普通类型
+    // 处理基本数据类型、null
+    else {
+        obj = target
+    }
+    return obj
+}
+
+// 浅拷贝
+function shallowClone(target) {
+    let obj
+    // 引用数据类型
+    if (typeof target === 'object' && target != null) {
+        obj = Array.isArray(target) ? [] : {}
+        for (const key in target) {
+            obj[key] = target[key]
+        }
+    }
+    // 普通数据类型、null
     else {
         obj = target
     }
