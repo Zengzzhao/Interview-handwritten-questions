b76bbcfdf1e94372d00ab6573efba72265b803e1
zengzizhao <787332942@qq.com>
2026-03-29 12:08:11 +0800

    01背包

---
+++ Algorithm​​/动态规划/背包问题/01背包.js
@@ -0,0 +1,49 @@
+const weight = [2, 2, 3, 1, 5, 2];
+const value = [2, 3, 1, 5, 4, 3];
+const size = 1;
+
+/**
+ * @param {number[]} weight 物品重量
+ * @param {number[]} value 物品价值
+ * @param {number} size 背包容量
+ */
+// 二维dp
+function bag01(weight, value, size) {
+  // dp[i][j]表示从0-i的物品中任选，放入容量j的背包中，所得到的最大价值
+  const dp = new Array(weight.length)
+    .fill()
+    .map(() => new Array(size + 1).fill(0));
+  // 初始化
+  for (let i = 0; i <= size; i++) {
+    dp[0][i] = size > weight[0] ? value[0] : 0;
+  }
+  // 遍历
+  for (let i = 1; i < weight.length; i++) {
+    for (let j = 1; j <= size; j++) {
+      if (size < weight[i]) {
+        // 放不下
+        dp[i][j] = dp[i - 1][j];
+      } else {
+        // 放得下
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i]);
+      }
+    }
+  }
+  return dp[weight.length - 1][size];
+}
+console.log(bag01(weight, value, size));
+
+// 一维dp
+function bag012(weight, value, size) {
+  // dp[i]表示背包容量为i时，装入的物品的最大价值
+  const dp = new Array(size + 1).fill(0);
+  // 遍历
+  for (let i = 0; i < weight.length; i++) {
+    for (let j = size; j >= weight[i]; j--) {
+      // j >= weight[i]已经表示当前进入到的是放得下的逻辑
+      dp[j] = Math.max(dp[j], dp[j - weight[i]] + value[i]);
+    }
+  }
+  return dp[size];
+}
+console.log(bag012(weight, value, size));
