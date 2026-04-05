4904175a4906d8d6a48a1bc42838ca53b23d77d9
zengzizhao <787332942@qq.com>
2026-04-05 09:54:12 +0800

    bind手写修改

--- js/this指向的三个函数/bind/index.js
+++ js/this指向的三个函数/bind/index.js
@@ -3,19 +3,23 @@
 
 // context为要绑定的this,...args为调用myBind时传递所绑定的参数
 Function.prototype.myBind = function (context, ...args) {
-    const fn = this
-    // ...newArgs为调用当前返回的绑定了this与参数后的函数时所传递的参数
-    return function (...newArgs) {
-        // 将fn所有参数拼接后运行
-        return fn.apply(context, args.concat(newArgs))
-    }
-}
+  const fn = this;
+  // ...rest为调用当前返回的绑定了this与参数后的函数时所传递的参数
+  return function (...rest) {
+    const fnSymbol = Symbol();
+    context[fnSymbol] = fn;
+    // 将fn所有参数拼接后运行
+    const result = context[fnSymbol](...args, ...rest);
+    delete context[fnSymbol];
+    return result;
+  };
+};
 
 function greet(msg1, msg2, msg3, msg4) {
-    console.log(this.name, msg1, msg2, msg3, msg4);
+  console.log(this.name, msg1, msg2, msg3, msg4);
 }
 const obj = {
-    name: 'zzz'
-}
-const bindGreet = greet.myBind(obj, 1, 2)
-bindGreet(3, 4)
+  name: "zzz",
+};
+const bindGreet = greet.myBind(obj, 1, 2);
+bindGreet(3, 4);
