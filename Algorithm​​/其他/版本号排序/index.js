e35f15e97775b9b1bfc807397f1b4330604037de
zengzizhao <787332942@qq.com>
2026-04-14 19:45:49 +0800

    版本号排序修改

--- Algorithm​​/其他/版本号排序/index.js
+++ Algorithm​​/其他/版本号排序/index.js
@@ -5,12 +5,11 @@ const version = ["0.1.1", "2.3.3", "0.3002.1", "4.2", "4.3.5", "4.3.4.5"];
 version.sort((a, b) => {
   const aParts = a.split(".").map(Number);
   const bParts = b.split(".").map(Number);
-  const len = Math.max(aParts.length, bParts.length);
-  for (let i = 0; i < len; i++) {
-    const curA = aParts[i] ?? 0;
-    const curB = bParts[i] ?? 0;
-    if (curA != curB) {
-      return curA - curB;
+  while (aParts.length < 3) aParts.push(0);
+  while (bParts.length < 3) bParts.push(0);
+  for (let i = 0; i < 3; i++) {
+    if (aParts[i] != bParts[i]) {
+      return aParts[i] - bParts[i];
     }
   }
   return 0;
